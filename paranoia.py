a104d602190149469215d699f2ec3b9c09101716
ArcticBabe <hector_espinoza02@hotmail.com>
2020-03-24 05:01:56 -0700

    Updated

--- paranoia.py
+++ paranoia.py
@@ -16,12 +16,15 @@ init(autoreset=True)
 if len(sys.argv) == 4:
     # Encrypt
     if sys.argv[1] == "-F" and sys.argv[2] == "-e" and sys.argv[3] != "":
-        file = Path(input("File to encrypt (use full path if necessary)\n> "))  # Turns the given file name to a windows path
-        password = sys.argv[3].encode()  # Gets the password and turns it to bytes
+        # Turns the given file name to a windows path
+        file = Path(input("File to encrypt (use full path if necessary)\n> "))
+        # Gets the password and turns it to bytes
+        password = sys.argv[3].encode()
         sleep(1)
         print("Starting...")
         EncryptedToken = defs.encrypt(password, 1, file)
-        file_name = path.dirname(path.abspath(file)) + "\encrypted.file"  # Gets the path of the file and creates the new one in there
+        # Gets the path of the file and creates the new one in there
+        file_name = path.dirname(path.abspath(file)) + "\encrypted.file"
 
         with open(file_name, "wb") as file_encryption:
             file_encryption.write(EncryptedToken)  # Writes the encrypted file
@@ -33,15 +36,20 @@ if len(sys.argv) == 4:
     # Decrypt
     elif sys.argv[1] == "-F" and sys.argv[2] == "-d" and sys.argv[3] != "":
         file = input("File to decrypt (use full path if necessary)\n> ")
-        file_name = input("New file name (include extension - Dont use full path)\n> ")
-        file_name = f"{path.dirname(path.abspath(file))}\{file_name}"  # Gets the path of the file and add the given name
-        password = sys.argv[3].encode()  # Gets the password and turns it to bytes
+        file_name = input(
+            "New file name (include extension - Dont use full path)\n> ")
+        # Gets the path of the file and add the given name
+        file_name = f"{path.dirname(path.abspath(file))}\{file_name}"
+        # Gets the password and turns it to bytes
+        password = sys.argv[3].encode()
         sleep(1)
         print("Starting...")
 
         with open(file, "r") as file_encrypted:
-            token = file_encrypted.read().encode()  # Reads the file and convert the string to bytes
-            DecryptedToken = defs.decrypt(password, token)  # Decryption of the file
+            # Reads the file and convert the string to bytes
+            token = file_encrypted.read().encode()
+            DecryptedToken = defs.decrypt(
+                password, token)  # Decryption of the file
             file_encrypted.close()
 
         with open(file_name, "wb") as final_file:  # Creates the decrypted file
@@ -52,8 +60,10 @@ if len(sys.argv) == 4:
         system("cls")
 
     elif sys.argv[1] == "-T" and sys.argv[2] == "-e" and sys.argv[3] != "":
-        data = input("What do you want to encrtpy?\n> ").encode()  # Turns the string given into bytes
-        password = sys.argv[3].encode()  # Grabs the password and turns it into bytes
+        # Turns the string given into bytes
+        data = input("What do you want to encrtpy?\n> ").encode()
+        # Grabs the password and turns it into bytes
+        password = sys.argv[3].encode()
         EncryptedToken = defs.encrypt(password, 2, data)  # Encrypts the data
         pyperclip.copy(EncryptedToken.decode())
         print(Fore.GREEN + """
@@ -63,10 +73,10 @@ if len(sys.argv) == 4:
 
     elif sys.argv[1] == "-T" and sys.argv[2] == "-d" and sys.argv[3] != "":
         data = input("What do you want to decrypt?\n> ").encode()
-        password = sys.argv[3].encode()  # Gets the password and turns it to bytes 
+        password = sys.argv[3].encode()  # Gets the password and turns it to bytes
         DecryptedToken = defs.decrypt(password, data)  # Decrypts the data
         print(f"""
-        Your decrypted data is: {Fore.GREEN + DecryptedToken.decode()} 
+        Your decrypted data is: {Fore.GREEN + DecryptedToken.decode()}
         {Fore.RESET + "And it has been copied to your clipboard"}.""")  # Turns the bytes to string so it can concatenate with Fore
         pyperclip.copy(DecryptedToken.decode())
         sleep(2)
